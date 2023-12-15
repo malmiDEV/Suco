c37d93c24002622718ffb587a05e18f184bc28da
malmiDEV <leeyoul9876@gmail.com>
2023-12-15 23:46:54 +0900

    build lexer

--- README.md
+++
@@ -1,2 +0,0 @@
-# Suco
-A Suco language. My own language that i create to write malmiOS
--- suco.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUCO_H
-#define SUCO_H
-
-#include "lexer.h"
-
-void compilation_unit(char *src);
-
-#endif
